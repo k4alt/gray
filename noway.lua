f21505076c39a2559d4d377fa1c2aef1a5186386
k4alt <134038093+k4alt@users.noreply.github.com>
2023-09-03 11:09:38 +1000

    Update noway.lua

--- noway.lua
+++ noway.lua
@@ -1,24 +1,5 @@
 if getgenv().Aiming then return getgenv().Aiming end
 
-if Rise.Main.Adonis_BP then
-local plr = game:GetService("Players").LocalPlayer
-local cclosure = syn_newcclosure or newcclosure or nil
-if not cclosure or not hookmetamethod then
-plr:Kick("\n\nYour exploit doesn't support hookmetamethod\n(x.synapse.to | script-ware.com)\n")
-end
-local oldNamecall
-oldNamecall = hookmetamethod(game, "namecall", cclosure(function(self,...)
-local NamecallMethod = getnamecallmethod()
-local args = {...}
-if (NamecallMethod == "Kick" or NamecallMethod == "kick") and not checkcaller() then
-   if self ~= plr then
-       return oldNamecall(self,...)
-   end
-   return
-end
-return oldNamecall(self,...)
-end))
-end
 
 
 -- // Services
