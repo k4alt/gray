7df7cc2d90cc9bcc8cc952234ce407b5a7e756c1
k4alt <134038093+k4alt@users.noreply.github.com>
2023-09-03 11:03:16 +1000

    Update noway.lua

--- noway.lua
+++ noway.lua
@@ -55,10 +55,13 @@ local tableremove = table.remove
 local tableinsert = table.insert
 
 if Rise.Silent.ClosestPart == true then
-   if Rise.Silent.HitParts == "8" then
-     getgenv().Rise.Silent.Part = {"Head", "UpperTorso", "HumanoidRootPart", "RightFoot", "LeftFoot"},
+   if Rise.Silent.HitParts == "4" then
+     getgenv().Rise.Silent.Part = {"Head", "UpperTorso", "HumanoidRootPart", "LowerTorso"},
    else
+   if Rise.Silent.HitParts == "8" then
      getgenv().Rise.Silent.Part = {"Head", "LeftArm", "RightArm", "UpperTorso", "LowerTorso", "RightFoot", "LeftFoot", "HumanoidRootPart"},
+   else
+     getgenv().Rise.Silent.Part = {"Head", "UpperTorso", "LowerTorso", "LeftFoot", "LeftLowerLeg", "LeftUpperLeg", "RightFoot", "RightLowerLeg", "RightUpperLeg", "LeftHand", "LeftLowerArm", "LeftUpperArm", "RightHand", "RightLowerArm", "RightUpperArm", "HumanoidRootPart"},
    end
 end
 -- // Silent Aim Vars
